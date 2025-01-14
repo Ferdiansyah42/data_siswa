18b946bb0fe69d92345859ad91f4e9c072dcb831
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-01-14 19:32:47 +0700

    fix

--- resources/views/buku/index.blade.php
+++ resources/views/buku/index.blade.php
@@ -13,6 +13,27 @@
                     <th>aksi</th>
                 </tr>
             </thead>
-            
+            <body>
+                @foreach ($allbuku as $key => $r)
+                <tr>
+                <td>{{$key +1 }}</td>
+                <td>{{$r->judul}}</td>
+                <td>{{$r->Pengarang}}</td>
+                <td>{{$r->Tahun_terbit}}</td>
+                <td>{{$r->penerbit->nama_penerbit}}</td>
+                <td>{{$r->kategori->nama_kategori}}</td>
+                <td>
+                    <form action="{{route('buku.destroy', $r->id) }}" method="POST">
+                        <a href="{{ route('buku.show', $r->id) }}" class="tombol">Detail</a>
+                        <a href="{{ route('buku.edit', $r->id) }}" class="tombol">Edit</a>
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="tombol">Hapus</button>
+
+                    </form>
+                </td>
+                </tr>
+                @endforeach
+            </body>
         </table>
 @include('layout.footer')
